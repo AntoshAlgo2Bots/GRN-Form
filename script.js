1948f35d04c6f819a7c1ff3a24ab3d803c16c06c
AntoshAlgo2Bots <antosh.pandey@algo2botsinfotech.com>
2024-08-30 20:09:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,134 +1,136 @@
-let received_txt = `<div class="flex flex-wrap items-center mt-3 border-black border-t pt-2">
-                         <div>
-                            <input id="link-checkbox" type="checkbox" value=""
-                                class="w-3 h-4 text-blue-600 bg-gray-100 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:border-gray-600">
+let received_txt = `<div class="flex flex-wrap gap-x-3 items-center mt-4 border-t ">
+                        <div>
+                            <input id="link-checkbox" type="checkbox" value="" 
+                                class="w-3 h-3 text-blue-600 bg-gray-100 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:border-gray-600">
                         </div>
-                        <div class="ml-2">
+                        <div class="">
                             <label class="block mb-2 font-bold text-xs font-medium text-gray-900 dark:text-white">S. No
                                 :
                             </label>
                             <input type="text" name="email" id="email"
-                                class="w-20 rounded-md border text-xs border-[#e0e0e0] bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                                class="w-20 rounded-md border text-xs border-gray-900 bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
                         </div>
-                        <div class="ml-2">
+                        <div class="">
                             <label class="block mb-2 font-bold text-xs font-medium text-gray-900 dark:text-white">Item
                                 Code:
                             </label>
                             <input type="text" name="email" id="email"
-                                class="w-28 rounded-md border text-xs border-[#e0e0e0] bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                                class="w-32 rounded-md border text-xs border-gray-900 bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
                         </div>
-                        <div class="ml-2">
+                        <div class="">
                             <label class="block mb-2 font-bold text-xs font-medium text-gray-900 dark:text-white">Item
                                 Desc :
                             </label>
                             <input type="text" name="email" id="email"
-                                class="w-40 rounded-md border text-xs border-[#e0e0e0] bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                                class="w-32 rounded-md border text-xs border-gray-900 bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
                         </div>
-                        <div class="ml-2">
+                        <div class="">
                             <label class="block mb-2 font-bold text-xs font-medium text-gray-900 dark:text-white">Qty :
                             </label>
                             <input type="text" name="email" id="email"
-                                class="w-24 rounded-md border text-xs border-[#e0e0e0] bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                                class="w-28 rounded-md border text-xs border-gray-900 bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
                         </div>
-                        <div class="ml-2">
+                        <div class="">
                             <label class="block mb-2 font-bold text-xs font-medium text-gray-900 dark:text-white">Qty
                                 Recv:
                             </label>
                             <input type="text" name="email" id="email"
-                                class="w-24 rounded-md border text-xs border-[#e0e0e0] bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                                class="w-28 rounded-md border text-xs border-gray-900 bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
                         </div>
-                        <div class="ml-2">
+                        <div class="">
                             <label class="block mb-2 font-bold text-xs font-medium text-gray-900 dark:text-white">SI
                                 Unit :
                             </label>
                             <input type="text" name="email" id="email"
-                                class="w-20 rounded-md border text-xs border-[#e0e0e0] bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                                class="w-28 rounded-md border text-xs border-gray-900 bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
                         </div>
-                        <div class="ml-2">
+                        <div class="">
                             <label class="block mb-2 font-bold text-xs font-medium text-gray-900 dark:text-white">Sub
                                 Inveory:
                             </label>
                             <input type="text" name="email" id="email"
-                                class="w-24 rounded-md border text-xs border-[#e0e0e0] bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                                class="w-28 rounded-md border text-xs border-gray-900 bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
                         </div>
-                        <div class="ml-2">
-                        <label class="block mb-2 font-bold text-xs font-medium text-gray-900 dark:text-white">Status:
-                            </label>
-                            <input type="text" name="email" id="email" placeholder="Received" disabled
-                            class="w-20 rounded-md border text-xs border-[#e0e0e0] bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md placeholder-black::placeholder" />
-                        </div>
+                        <div class="">
+    <label class="block mb-2 font-bold text-xs font-medium text-gray-900 dark:text-white">Status:
+    </label>
+    <input type="text" name="email" id="email" placeholder="Received" disabled
+        class="w-20 rounded-md border text-xs border-[#e0e0e0] bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md placeholder-black::placeholder" />
+</div>
                     </div>`
 
-let reject_txt = `<div class="flex flex-wrap items-center mt-3 border-black border-t pt-2">
+let reject_txt = `<div class="flex flex-wrap gap-x-3 items-center mt-3 border-black border-t pt-2">
                         <div>
-                            <input id="link-checkbox" type="checkbox" value="" class="w-3 h-4 text-blue-600 bg-gray-100 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:border-gray-600">
+                            <input id="link-checkbox" type="checkbox" value=""
+                                class="w-3 h-3 text-blue-600 bg-gray-100 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:border-gray-600">
                         </div>
-                        <div class="ml-2">
+                        <div class="">
                             <input type="text" name="email" id="email"
-                            class="w-20 rounded-md border text-xs border-[#e0e0e0] bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                                class="w-20 rounded-md border text-xs border-gray-900 bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
                         </div>
-                        <div class="ml-2">
+                        <div class="">
                             <input type="text" name="email" id="email"
-                            class="w-28 rounded-md border text-xs border-[#e0e0e0] bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                                class="w-32 rounded-md border text-xs border-gray-900 bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
                         </div>
-                        <div class="ml-2">
+                        <div class="">
                             <input type="text" name="email" id="email"
-                            class="w-40 rounded-md border text-xs border-[#e0e0e0] bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                                class="w-32 rounded-md border text-xs border-gray-900 bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
                         </div>
-                        <div class="ml-2">
+                        <div class="">
                             <input type="text" name="email" id="email"
-                            class="w-24 rounded-md border text-xs border-[#e0e0e0] bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                                class="w-28 rounded-md border text-xs border-gray-900 bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
                         </div>
-                        <div class="ml-2">
+                        <div class="">
                             <input type="text" name="email" id="email"
-                            class="w-24 rounded-md border text-xs border-[#e0e0e0] bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                                class="w-28 rounded-md border text-xs border-gray-900 bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
                         </div>
-                        <div class="ml-2">
+                        <div class="">
                             <input type="text" name="email" id="email"
-                            class="w-20 rounded-md border text-xs border-[#e0e0e0] bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                                class="w-28 rounded-md border text-xs border-gray-900 bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
                         </div>
-                        <div class="ml-2">
-                            <input type="text" name="email" id="email" placeholder="Damage" disabled
-                            class="w-24 rounded-md border text-xs border-[#e0e0e0] bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                        <div class="">
+                            <input type="text" name="email" id="email" placeholder="Demage" disabled
+                                class="w-28 rounded-md border text-xs border-[#e0e0e0] bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
                         </div>
-                        <div class="ml-2">
+                        <div class="">
                             <input type="text" name="email" id="email" placeholder="Rejected" disabled
                             class="w-20 rounded-md border text-xs border-[#e0e0e0] bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md placeholder-black::placeholder" />
                         </div>
                     </div>`
 
-let accept_txt = `<div class="flex flex-wrap items-center mt-3 border-black border-t pt-2">
-                                            <div>
-                                                <input id="link-checkbox" type="checkbox" value="" class="w-3 h-4 text-blue-600 bg-gray-100 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:border-gray-600">
-                                            </div>
-                                            <div class="ml-2">
-                                                <input type="text" name="email" id="email"
-                                                class="w-20 rounded-md border text-xs border-[#e0e0e0] bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
-                                            </div>
-                                            <div class="ml-2">
-                                                <input type="text" name="email" id="email"
-                                                class="w-28 rounded-md border text-xs border-[#e0e0e0] bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
-                                            </div>
-                                            <div class="ml-2">
-                                                <input type="text" name="email" id="email"
-                                                class="w-40 rounded-md border text-xs border-[#e0e0e0] bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
-                                            </div>
-                                            <div class="ml-2">
-                                                <input type="text" name="email" id="email"
-                                                class="w-24 rounded-md border text-xs border-[#e0e0e0] bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
-                                            </div>
-                                            <div class="ml-2">
-                                                <input type="text" name="email" id="email"
-                                                class="w-24 rounded-md border text-xs border-[#e0e0e0] bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
-                                            </div>
-                                            <div class="ml-2">
-                                                <input type="text" name="email" id="email"
-                                                class="w-20 rounded-md border text-xs border-[#e0e0e0] bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
-                                            </div>
-                                            <div class="ml-2">
-                                                <input type="text" name="email" id="email"
-                                                class="w-24 rounded-md border text-xs border-[#e0e0e0] bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
-                                            </div>
+let accept_txt = `<div class="flex flex-wrap items-center gap-x-3 mt-3 border-black border-t pt-2">
+                                           <div>
+                            <input id="link-checkbox" type="checkbox" value=""
+                                class="w-3 h-3 text-blue-600 bg-gray-100 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:border-gray-600">
+                        </div>
+                        <div class="">
+                            <input type="text" name="email" id="email"
+                                class="w-20 rounded-md border text-xs border-gray-900 bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                        </div>
+                        <div class="">
+                            <input type="text" name="email" id="email"
+                                class="w-32 rounded-md border text-xs border-gray-900 bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                        </div>
+                        <div class="">
+                            <input type="text" name="email" id="email"
+                                class="w-32 rounded-md border text-xs border-gray-900 bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                        </div>
+                        <div class="">
+                            <input type="text" name="email" id="email"
+                                class="w-28 rounded-md border text-xs border-gray-900 bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                        </div>
+                        <div class="">
+                            <input type="text" name="email" id="email"
+                                class="w-28 rounded-md border text-xs border-gray-900 bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                        </div>
+                        <div class="">
+                            <input type="text" name="email" id="email"
+                                class="w-28 rounded-md border text-xs border-gray-900 bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                        </div>
+                        <div class="">
+                            <input type="text" name="email" id="email"
+                                class="w-28 rounded-md border text-xs border-gray-900 bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                        </div>
                                             <div class="ml-2">
                                                 <input type="text" name="email" id="email" placeholder="Accepted" disabled
                                                 class="w-20 rounded-md border text-xs border-[#e0e0e0] bg-white py-3 pl-2 text-[#6B7280] h-6 outline-none focus:border-[#6A64F1] focus:shadow-md placeholder-black::placeholder" />
@@ -152,12 +154,22 @@ function received() {
 function accept() {
     let checkbox = document.getElementById('link-checkbox');
     let accept_div = document.getElementById('accept_div');
-
+    
     if (checkbox.checked) {
         accept_div.innerHTML += accept_txt;
     } else {
         console.log("without check");
     }
+    
+    
+    let lot_btn = document.getElementById('lot_btn')
+
+    if (lot_btn.style.display === "none") {
+        lot_btn.style.display = "block";
+    } else {
+        lot_btn.style.display = "block";
+    }
+
 
     console.log("finish");
 }
@@ -215,3 +227,18 @@ function view_section() {
     }
 }
 
+
+
+
+function form_radio(){
+    let x=document.getElementById("serial_from")
+    let y=document.getElementById("serial_to")
+    let z=document.getElementById("lot_no")
+
+    if(z.checked){
+        x.innerHTML = "Lot Number From"
+        y.innerHTML = "Lot Number To"
+    }
+
+    console.log("hey")
+}
